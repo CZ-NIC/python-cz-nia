a3bf3e00c448696d54137f431fc79a6bb804f65a
tpazderka <tomas.pazderka@nic.cz>
2018-11-07 15:39:14 +0100

    Fix project metadata (#8)
    * Fix project metadata

--- setup.py
+++ setup.py
@@ -6,7 +6,12 @@ import cz_nia
 
 setup(name='cz_nia',
       version=cz_nia.__version__,
+      author='Tomas Pazderka',
+      author_email='tomas.pazderka@nic.cz',
+      url='https://github.com/tpazderka/python-cz-nia',
       description='Python application for communication with Czech NIA.',
+      long_description=open('README.md').read(),
+      long_description_content_type='text/markdown',
       packages=find_packages(),
       install_requires=['zeep', 'xmlsec'],
       extras_require={'quality': ['isort', 'flake8', 'pydocstyle']})
