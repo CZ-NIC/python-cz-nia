26bac36d0bf362576eb646767c5735f32a3a871d
Tomas Pazderka <tomas.pazderka@nic.cz>
2019-05-09 09:19:24 +0200

    Fix for new isort

--- cz_nia/wsse/__init__.py
+++ cz_nia/wsse/__init__.py
@@ -1,5 +1,4 @@
 """Module with WSSE extensions."""
 from .signature import BinarySignature, MemorySignature, SAMLTokenSignature, Signature
 
-
 __all__ = ['BinarySignature', 'MemorySignature', 'SAMLTokenSignature', 'Signature']
